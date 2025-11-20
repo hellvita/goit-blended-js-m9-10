8e77929ba371432297a3953118bf2777621dcdc5
hellvita <seredaolga03@gmail.com>
2025-11-20 19:24:17 +0200

    update config base value

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/goit-blended-js-m9-10/',
     root: 'src',
     build: {
       sourcemap: true,
